9344467c38af53fcf57fff81243c0ea760804ec1
zhangjingguang <2586300277@qq.com>
2024-10-22 17:40:03 +0800

    change

--- index.php
+++ index.php
@@ -3,35 +3,46 @@
 $html = file_get_contents('index.html');
 
 try {
+    $seo = json_decode(file_get_contents('seo.json'), true);
 
-    $seo = file_get_contents('seo.json');
-
-    $seo = json_decode($seo, true);
+    if ($seo === null) {
+        echo $html;
+    }
 
     $rand_nav = $seo[array_rand($seo)];
-
     $examples = $rand_nav['examples'];
-
     $rand_example = $examples[array_rand($examples)];
-
     $classify = $rand_example['children'];
-
     $rand_classify = $classify[array_rand($classify)];
-
     $meta = $rand_classify['meta'] ?? [];
 
-    if (isset($meta['title']))  $html = preg_replace('/<title>.*<\/title>/', '<title>' . $meta['title'] . '</title>', $html);
+    $title = $meta['title'] ?? '开源三维' . date('Y/m/d/H');
+    $keywords = $meta['keywords'] ?? '';
+    $description = $meta['description'] ?? '';
 
-    else $html = preg_replace('/<title>.*<\/title>/', '<title>' . '开源三维' . date('Y/m/d/H') . '</title>', $html);
+    // 使用 htmlspecialchars 转义
+    $title = htmlspecialchars($title, ENT_QUOTES);
+    $keywords = htmlspecialchars($keywords, ENT_QUOTES);
+    $description = htmlspecialchars($description, ENT_QUOTES);
 
-    if (isset($meta['keywords']))  $html = preg_replace('/<meta name="keywords" content=".*" \/>/', '<meta name="keywords" content="' . $meta['keywords'] . '" />', $html);
+    // 更新 HTML 内容
+    $html = preg_replace('/<title>.*<\/title>/', '<title>' . $title . '</title>', $html);
+    $html = preg_replace('/<meta name="keywords" content=".*" \/>/', '<meta name="keywords" content="' . $keywords . '" />', $html);
+    $html = preg_replace('/<meta name="description" content=".*" \/>/', '<meta name="description" content="' . $description . '" />', $html);
 
-    if (isset($meta['description']))  $html = preg_replace('/<meta name="description" content=".*" \/>/', '<meta name="description" content="' . $meta['description'] . '" />', $html);
+    // 添加结构化数据
+    $structuredData = [
+        "@context" => "http://threehub.cn",
+        "@type" => "WebPage",
+        "name" => $title,
+        "description" => $description,
+        "keywords" => $keywords
+    ];
+    $structuredDataScript = '<script type="application/ld+json">' . json_encode($structuredData, JSON_UNESCAPED_UNICODE) . '</script>';
+    $html = str_replace('</head>', $structuredDataScript . '</head>', $html);
 
     echo $html;
 
 } catch (Exception $e) {
-
     echo $html;
-    
 }
